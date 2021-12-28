ddb22ca369fe2625187d357d5ae0f029650e6c79
massznlasr <84396840+massznlasr@users.noreply.github.com>
2021-12-29 04:11:39 +0800

    Add files via upload

---
+++ storage/framework/views/b9fe84544273186667c21cfbe00d916e7025f5dc.php
@@ -0,0 +1,128 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/features/">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <!-- Bootstrap core CSS -->
+<!-- <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">-->
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="features.css" rel="stylesheet">
+  </head>
+  <body>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+<section class="py-5 text-center container">
+
+    <div class="card-body" style="background-image: url(<?php echo e(url('images/header.png')); ?>);">
+  
+    <div class="row py-lg-5">
+      <div class="col-lg-6 col-md-8 mx-auto">
+        <div><img src="images/masbaru1.jpg" class="pr-5 bd-placeholder-img rounded-circle" width="140" height="140"></div>
+        <h2 class="fw-light">Mastura Binti Zainul Asri</h2>
+        <p class="lead text-muted">Our websites offers variety of laundry services to helps you alleviate house chores and gives you solution to your door step</p>
+      </div>
+
+    </div>
+  </div>
+
+  </section>
+  
+
+ <div class="album py-5 bg-light">
+   <div class="container">
+
+
+<!--<div class="container marketing">-->
+
+      <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Regular Schedule </h2>
+        <p class="lead">Choose your pick up day and time and we'll come to your door the same day of each week or every other week depending on our runner. </p>
+      </div>
+      <div class="col-md-5">
+        <img src="images/schedule.png" width="400" height="400">
+        <!--<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>-->
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-2">
+        <h2 class="featurette-heading">Professional Cleaning </h2>
+        <p class="lead">Your pockets will be checked for anything that can ruin your cloth, and your lights and dark's will be washed separately. Also your items will be cleaned separately from other customers.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img src="images/pro.png" width="400" height="400" preserveAspectRatio="xMidYMid slice">
+        <!--<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>-->
+
+      </div>
+    </div>
+
+    <hr class="featurette-divider">
+
+    <div class="row featurette">
+      <div class="col-md-7">
+        <h2 class="featurette-heading">Free Laundry Bag </h2>
+        <p class="lead">There's no weight limit for a basic laundry services. What you fit in the bag it will be washed. All services include 2 days standard delivery.</p>
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img src="images/bag.png" width="400" height="400">
+         <!--<svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 500x500" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg> -->
+
+      </div>
+    </div>
+
+    
+
+    <!-- /END THE FEATURETTES -->
+    </div>
+</div>
+
+<div class="b-example-divider">
+<div class="container">
+  <footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+      <li class="nav-item"><a href="/" class="nav-link px-2 text-muted">Home</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Services</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Pricing</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">About</a></li>
+    </ul>
+    <p class="text-center text-muted">&copy; 2021 Dobi, Inc</p>
+  </footer>
+</div>
+</div>
+</body>
+
+<?php /**PATH C:\Users\MASTURA\Desktop\Laravel\portfolio\resources\views/welcome.blade.php ENDPATH**/ ?>
